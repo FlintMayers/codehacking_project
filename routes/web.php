1ee775cbfc1ae7f7cdd6ac6f673872982a5477d5
triangle <aphrodisiac@protonmail.com>
2017-06-08 15:12:47 +0300

    admin masterpage setup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/adminas', function(){
+	return view('admin.index');
+});
+
+Route::resource('admin/users', 'AdminUsersController');
